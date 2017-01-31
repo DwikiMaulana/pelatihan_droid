96219d4f470c2e8a465e996cd2bda32897406f92
DwikiMaulana <mvixion999@gmail.com>
2017-01-31 10:27:04 +0700

    Class Java Init

--- classjava/app/src/main/java/maulanadwiki/classjava/MainActivity.java
+++ classjava/app/src/main/java/maulanadwiki/classjava/MainActivity.java
@@ -2,12 +2,41 @@ package maulanadwiki.classjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public String showString;
+    public TextView showText;
+    public Barang [] arrBarang = new Barang[10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showText = (TextView) findViewById(R.id.show_text);
+
+        showString = "Manipulasi Class Java Android";
+        addSeparator();
+
+        initBarang();
+        showString += arrBarang[5].toString();
+        addSeparator();
+        showText.setText(showString);
+    }
+    public void addSeparator(){
+        showString += "\n--------------------------\n";
+    }
+
+    public void initBarang(){
+        arrBarang[0] = new Barang("Laptop", 1, 7000000);
+        arrBarang[1] = new Barang("Printer", Barang.ELEKTRONIK, 250000);
+        arrBarang[2] = new Barang("Monitor", Barang.ELEKTRONIK, 150000);
+        arrBarang[3] = new Barang("PE28 TDR", Barang.NON_ELEKTRONIK, 1200000);
+        arrBarang[4] = new Barang("IRC FASTI2",  Barang.NON_ELEKTRONIK, 350000);
+        arrBarang[5] = new Barang("PISTON SET GL PRO NEOTECH", Barang.NON_ELEKTRONIK, 300000);
+        arrBarang[6] = new Barang("GEAR SET SSS 428",  Barang.NON_ELEKTRONIK, 500000);
+        arrBarang[7] = new Barang("OLI",  Barang.NON_ELEKTRONIK, 50000);
+        arrBarang[8] = new Barang("KOP BURING SATRIA F",  Barang.NON_ELEKTRONIK, 900000);
+        arrBarang[9] = new Barang("DOP",  Barang.NON_ELEKTRONIK, 5000);
     }
 }
